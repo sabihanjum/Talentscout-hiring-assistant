a5531269c8228d7d1d5dd666eb68d4437d5ce232
Sabiha Anjum <drsabaanjum99@gmail.com>
2025-03-18 14:53:43 +0530

    Commit

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 def ask_gemini(prompt):
     try:
